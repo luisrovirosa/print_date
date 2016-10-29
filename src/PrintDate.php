8da1d60ba89095c710dd681861d000ece1fda007
Luis Rovirosa <luisrovirosa@gmail.com>
2016-10-29 11:40:21 +0200

    refactor: extract field: date

--- src/PrintDate.php
+++ src/PrintDate.php
@@ -4,11 +4,14 @@ namespace LuisRovirosa\PrintDate;
 
 class PrintDate
 {
+    /** @var Date */
+    private $date;
     /** @var Printer */
     private $printer;
 
     public function __construct(Printer $printer)
     {
+        $this->date = new Date();
         $this->printer = $printer;
     }
 
@@ -19,9 +22,7 @@ class PrintDate
 
     private function currentDate():string
     {
-        $date = new Date();
-
-        return $date->currentDate();
+        return $this->date->currentDate();
     }
 
     private function print($date)
